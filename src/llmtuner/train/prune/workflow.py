d6a160356889f5a65d82c8303f440020c77f1405
shwaihe <shwai.he@gmail.com>
2024-06-24 15:34:22 -0700

    upload

--- src/llmtuner/train/prune/workflow.py
+++ src/llmtuner/train/prune/workflow.py
@@ -1,7 +1,5 @@
 
 import os
-# import sys
-
 from copy import deepcopy
 from typing import TYPE_CHECKING, List, Optional
 
@@ -12,13 +10,8 @@ from torch.utils.data import DataLoader
 from global_utils.io import load_json, save_json
 from transformers import DataCollatorForSeq2Seq, DataCollatorForLanguageModeling, DataCollatorWithPadding
 from .block_drop import consecutive_block_dropping, discrete_block_dropping, post_block_drop
-# from .decompose import decompose_moe
-# from .expert_drop import layerwise_pruning, progressive_pruning, dynamic_skipping, global_pruning, post_experts_drop
-# from .gate_remap import gate_remap
 from .io import save_sparse_model, save_update_state_dict, save_decomposed_model, save_expert_dropped_config, save_block_dropped_config, save_layer_dropped_config
 from .layer_drop import discrete_layer_dropping, post_layers_drop
-# from ..dpo.collator import DPODataCollatorWithPadding
-# from ..rm.collator import PairwiseDataCollatorWithPadding
 from ...data import get_dataset
 from ...extras.constants import IGNORE_INDEX
 from ...model import load_model_and_tokenizer
@@ -30,13 +23,6 @@ if TYPE_CHECKING:
     from ...hparams import DataArguments, FinetuningArguments, ModelArguments, PruningArguments
 
 DATA_AWARE_PRUNING_METHODS = ("wanda", "sparsegpt", "gradient-first", "gradient-zeroth", "expert_drop", "block_drop", "layer_drop")
-
-# EXPERT_DROP_METHODS_FUNC = {
-#     'layerwise_pruning': layerwise_pruning,
-#     'global_pruning': global_pruning,
-#     'progressive_pruning': progressive_pruning,
-#     'dynamic_skipping': dynamic_skipping,
-# }
 
 LAYER_DROP_METHODS_FUNC = {
     'discrete': discrete_layer_dropping,
@@ -66,13 +52,6 @@ def run_prune(
 
     # 🔍 model & tokenizer
     model, tokenizer = load_model_and_tokenizer(model_args, finetuning_args, training_args.do_train)
-
-    # if pruning_args.prune_method == "expert_drop" and pruning_args.expert_drop_method == "post_dropping":
-    #     assert (os.environ.get("ACCELERATE_USE_DEEPSPEED", "false")) and (os.environ.get("ACCELERATE_USE_FSDP", "false"))
-    #     config = load_json(os.path.join(pruning_args.prune_model_save_path, "config.json"))
-    #     accelerator.wait_for_everyone()
-    #     post_experts_drop(pruning_args.prune_model_save_path, model, tokenizer, config, accelerator, preserve_gate=pruning_args.preserve_gate)
-    #     exit()
     
     if pruning_args.prune_method == "layer_drop" and pruning_args.layer_drop_method == "post_dropping":
         assert (os.environ.get("ACCELERATE_USE_DEEPSPEED", "false")) and (os.environ.get("ACCELERATE_USE_FSDP", "false"))
@@ -82,8 +61,6 @@ def run_prune(
     if pruning_args.prune_method == "block_drop" and pruning_args.block_drop_method == "post_dropping":
 
         assert (os.environ.get("ACCELERATE_USE_DEEPSPEED", "false")) and (os.environ.get("ACCELERATE_USE_FSDP", "false"))
-        # layer_id_mapping = load_json(os.path.join(pruning_args.prune_model_save_path, "layer_mapping.json"))
-        # post_block_drop(pruning_args.prune_model_save_path, model, tokenizer, layer_id_mapping, accelerator)
         reserved_layer_list = load_json(os.path.join(pruning_args.prune_model_save_path, "reserved_layers.json"))
         post_block_drop(pruning_args.prune_model_save_path, model, tokenizer, reserved_layer_list, accelerator, pruning_args.only_update_config)
         exit()
@@ -100,17 +77,6 @@ def run_prune(
                 pad_to_multiple_of=8 if tokenizer.padding_side == "right" else None,  # for shift short attention
                 label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
             )
-        # elif pruning_args.prune_data_type == "rm":
-        #     data_collator = PairwiseDataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
-        # elif pruning_args.prune_data_type == "ppo":
-        #     tokenizer.padding_side = "left"  # use left-padding in generation while using right-padding in training
-        #     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-        # else:  # dpo
-        #     data_collator = DPODataCollatorWithPadding(
-        #         tokenizer=tokenizer,
-        #         pad_to_multiple_of=8,
-        #         label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
-        #     )
 
         dataloader = DataLoader(dataset, batch_size=1, collate_fn=data_collator, num_workers=8)  # batch size must be 1
 
@@ -118,8 +84,7 @@ def run_prune(
         accelerator.print("Total Used Sample Num:", pruning_args.n_calibration_samples)
         accelerator.print("Max sequence Length:", data_args.cutoff_len)
         accelerator.print(f"Example Data (len = {len(dataset[0]['input_ids'])}):", dataset[0])
-        # print(pruning_args.n_calibration_samples)
-        # print(len(dataset))
+
         if pruning_args.n_calibration_samples > len(dataset):
             raise ValueError("Number of calibration samples is greater than the number of samples in the dataset!")
 
@@ -150,7 +115,6 @@ def run_prune(
 
     if pruning_args.prune_method == "wanda":
         update_state_dict = prune_wanda(pruning_args, model, dataloader, accelerator, num_samples_each_device, prune_n=prune_n, prune_m=prune_m)
-        # update_state_dict = prune_wanda_moe(pruning_args, model, dataloader, accelerator, num_samples_each_device, prune_n=prune_n, prune_m=prune_m)
     elif pruning_args.prune_method == "sparsegpt":
         update_state_dict = prune_sparsegpt(pruning_args, model, dataloader, accelerator, num_samples_each_device, prune_n=prune_n, prune_m=prune_m)
     elif pruning_args.prune_method == "gradient-first":
@@ -159,10 +123,6 @@ def run_prune(
         raise NotImplementedError
     elif pruning_args.prune_method == "magnitude":
         update_state_dict = prune_magnitude(pruning_args, model, accelerator, prune_n=prune_n, prune_m=prune_m)  # Data-independent
-    # elif pruning_args.prune_method == "decompose_moe":
-    #     update_state_dict = decompose_moe(pruning_args, model, accelerator)  # Data-independent
-    # elif pruning_args.prune_method == "expert_drop":
-    #     EXPERT_DROP_METHODS_FUNC[pruning_args.expert_drop_method](pruning_args, model, dataloader, accelerator, num_samples_each_device)
     elif pruning_args.prune_method == "layer_drop":
         dropped_layer_list = LAYER_DROP_METHODS_FUNC[pruning_args.layer_drop_method](pruning_args, model, dataloader, accelerator, num_samples_each_device)
     elif pruning_args.prune_method == "block_drop":
@@ -174,14 +134,6 @@ def run_prune(
     accelerator.print(f"model: {model}")
 
     if pruning_args.prune_model_save_path is not None:
-        # if pruning_args.prune_method == "decompose_moe":
-        #     # 🔍 Set config for low-rank decomposition.
-        #     setattr(accelerator.unwrap_model(model).config, "decomposed", True)
-        #     setattr(accelerator.unwrap_model(model).config, "has_sparse", pruning_args.has_sparse)
-        #     save_decomposed_model(pruning_args.prune_model_save_path, model, tokenizer, accelerator, update_state_dict)
-        # elif pruning_args.prune_method == "expert_drop":
-        #     # 🔍 only return the idx of remaining experts.
-        #     save_expert_dropped_config(pruning_args.prune_model_save_path, model, tokenizer, accelerator)
         if pruning_args.prune_method == "layer_drop":
             save_layer_dropped_config(pruning_args.target_layer, pruning_args.prune_model_save_path, model, tokenizer, accelerator, dropped_layer_list=dropped_layer_list)
         elif pruning_args.prune_method == "block_drop":
@@ -216,11 +168,8 @@ def run_prune_remap_gate(
     model_pruned_args = deepcopy(model_args)
     model_pruned_args.model_name_or_path = pruning_args.pruned_model_path
     model_pruned, _ = load_model_and_tokenizer(model_pruned_args, finetuning_args, training_args.do_train)
-
-    # tokenizer = load
     # 🔍 dataset & data collator & dataloader
     dataset = get_dataset(tokenizer, model_args, data_args, training_args, stage=pruning_args.prune_data_type)
-
     if pruning_args.prune_data_type == "pt":
         data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)  # concat all data to seq_length for each batch
     elif pruning_args.prune_data_type == "sft":
@@ -261,11 +210,7 @@ def run_prune_remap_gate(
     accelerator.print("Number of used samples per device:", num_samples_each_device)
 
     #######################################################################################################
-    # update_state_dict = gate_remap(model, model_pruned, dataloader, accelerator, num_samples_each_device)
-    #######################################################################################################
-
     print(model_pruned)
-
     # Updating the parameters from the state_dict will cause errors in the FSDP environment.
     # So we need to initialize a new model to load it.
     # Here we save the state_dict first to avoid accidents.
@@ -280,7 +225,5 @@ def run_prune_remap_gate(
         model_pruned.load_state_dict(update_state_dict, strict=False)
         model_pruned.save_pretrained(pruning_args.prune_model_save_path)
         tokenizer.save_pretrained(pruning_args.prune_model_save_path)
-        # delete_file_or_dir(os.path.join(pruning_args.prune_model_save_path, "update_state_dict.pt"))
     accelerator.wait_for_everyone()
-
     accelerator.print("All done!")
